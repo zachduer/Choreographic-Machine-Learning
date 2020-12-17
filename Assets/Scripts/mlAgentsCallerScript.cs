1f6de8c84ce1b335a1861e88afc1460aaa568d9b
slally17 <slally17@vt.edu>
2020-12-17 13:01:00 -0500

    mlAgents Caller Script Finished.

--- Assets/Scripts/mlAgentsCallerScript.cs
+++ Assets/Scripts/mlAgentsCallerScript.cs
@@ -1,24 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Diagnostics;
 using System.Threading;
 
 public class mlAgentsCallerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    string pythonFileName;
+    string scriptFileName;
+    string brainFileName;
+    string settings;
+
+    Thread mlAgentsThread; // thread opened to call kinect script.
+
     void Start()
     {
-        
+        pythonFileName = Application.persistentDataPath;        
+        string[] parts = pythonFileName.Split('/');
+        pythonFileName = parts[0] + "/" + parts[1] + "/" + parts[2] + "/" + parts[3] + "/";
+        pythonFileName += "Local/Programs/Python/Python36/";
+        scriptFileName = pythonFileName;
+        pythonFileName += "python.exe";
+        scriptFileName += "Lib/site-packages/mlagents/trainers/learn.py";
+
+        brainFileName = Application.dataPath + "/BrainConfig/motionCaptureRNN.yaml";
+        settings = "--run-id=sometestname --force --time-scale 1";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void buttonPress()
     {
-        
+        UnityEngine.Debug.Log("mlagents script clicked.");
+        mlAgentsThread = new Thread(new ThreadStart(startMlAgentsScript));
+        mlAgentsThread.IsBackground = true;
+        mlAgentsThread.Start();
     }
 
     void startMlAgentsScript()
     {
+        ProcessStartInfo start = new ProcessStartInfo();
+        start.FileName = pythonFileName;
+        start.Arguments = string.Format("{0} {1} {2}", scriptFileName, brainFileName, settings); //mlagentsProgram brainYaml runSettings
+        start.WindowStyle = ProcessWindowStyle.Hidden;
+        start.CreateNoWindow = true;
+        int exitCode;
 
+        using (Process proc = Process.Start(start))
+        {
+            proc.WaitForExit();
+            exitCode = proc.ExitCode;
+        }
+        UnityEngine.Debug.Log(exitCode);
     }
 }
